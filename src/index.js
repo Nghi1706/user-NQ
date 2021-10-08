c2e32f8508323dd72f36bd2e07b9165b7fdc8bfd
Nghi <nguyenvannghi17062000@gmail.com>
2021-10-08 20:39:43 +0700

    return chat

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import reducers from './redux/reducers';
 import mySaga from './redux/sagas';
-import Chat from './layout/chat'
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(reducers, applyMiddleware(sagaMiddleware));
 
@@ -16,7 +15,6 @@ sagaMiddleware.run(mySaga);
 ReactDOM.render(
   <Provider store={store}>
     <App />
-    <Chat></Chat>
   </Provider>,
   document.getElementById('root')
 );
